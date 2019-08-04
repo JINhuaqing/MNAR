c332476c38e4b6b76758b23d6a88b3dbd15cd15e
JINhuaqing <kevinjin0423@gmail.com>
2019-08-04 07:50:56 -0400

    add simulation for truncted normal

--- MissDepRG_normal.py
+++ MissDepRG_normal.py
@@ -5,6 +5,7 @@ import torch
 import pickle
 import timeit
 import time
+from scipy.stats import norm as STN
 
 torch.manual_seed(0) # cpu
 torch.cuda.manual_seed(0) #gpu
@@ -19,39 +20,93 @@ if cuda:
 sigma = 0.5
 
 
-def fn(y, m, bsXs=None, sigma=sigma):
+def torchstnpdf(y):
+    pi = torch.tensor([np.pi])
+    prefix = torch.sqrt(1/pi/2)
+    return prefix*torch.exp(-y**2/2)
+
+# def fn(y, m, bsXs=None, sigma=sigma):
+#     # y     : n x m
+#     # m     : n x m 
+#     # bsXs  : N
+#     pi = torch.tensor([np.pi])
+#     prefix = torch.sqrt(1/pi/2/sigma**2)
+#     if bsXs is not None:
+#         v = torch.exp(-(y.unsqueeze(-1)- (m.unsqueeze(-1) + bsXs))**2/2/sigma**2)
+#     else:
+#         v = torch.exp(-(y-m)**2/2/sigma**2)
+#     return prefix*v
+# 
+# 
+# def fn2(y, m, bsXs=None, sigma=sigma):
+#     pi = torch.tensor([np.pi])
+#     sigma2 = sigma**2
+#     prefix = torch.sqrt(1/pi/2/sigma2)
+#     #v1 = torch.exp(-(y-m)**2/2/sigma2)
+#     #v2 =  (y-m)/sigma2
+#     if bsXs is not None:
+#         return prefix*torch.exp(-(y.unsqueeze(-1)- (m.unsqueeze(-1) + bsXs))**2/2/sigma2)*(y.unsqueeze(-1)- (m.unsqueeze(-1) + bsXs))/sigma2
+#     else:
+#         return prefix*torch.exp(-(y-m)**2/2/sigma2)*(y-m)/sigma2
+# 
+# 
+# def fn22(y, m, sigma=sigma):
+#     pi = torch.tensor([np.pi])
+#     sigma2 = sigma**2
+#     prefix = torch.sqrt(1/pi/2/sigma2)
+#     expitm = torch.exp(-(y-m)**2/2/sigma2)
+#     v =  (y-m)/sigma2
+#     linitm = v**2-1/sigma2
+#     return prefix*linitm*expitm
+
+
+def ftn(y, m, bsXs=None, sigma=sigma, a=-100, b=100):
     # y     : n x m
     # m     : n x m 
     # bsXs  : N
-    pi = torch.tensor([np.pi])
-    prefix = torch.sqrt(1/pi/2/sigma**2)
+    print(bsXs.shape, m.shape)
+    Z = (STN.cdf(b/sigma) - STN.cdf(a/sigma)) * sigma
     if bsXs is not None:
-        v = torch.exp(-(y.unsqueeze(-1)- (m.unsqueeze(-1) + bsXs))**2/2/sigma**2)
+        v = (y.unsqueeze(-1)- (m.unsqueeze(-1) + bsXs))/sigma
+        zidx = (y.unsqueeze(-1) > b + (m.unsqueeze(-1) + bsXs)) | (y.unsqueeze(-1) < a + (m.unsqueeze(-1) + bsXs))
     else:
-        v = torch.exp(-(y-m)**2/2/sigma**2)
-    return prefix*v
+        v = (y-m)/sigma
+        zidx = (y > b + m) | (y < a+m)
+    tv = torchstnpdf(v)/Z 
+    tv[zidx] = 0
+    return tv
 
 
-def fn2(y, m, bsXs=None, sigma=sigma):
-    pi = torch.tensor([np.pi])
+def ftn2(y, m, bsXs=None, sigma=sigma, a=-100, b=100):
     sigma2 = sigma**2
-    prefix = torch.sqrt(1/pi/2/sigma2)
-    #v1 = torch.exp(-(y-m)**2/2/sigma2)
-    #v2 =  (y-m)/sigma2
+    Z = (STN.cdf(b/sigma) - STN.cdf(a/sigma)) * sigma
     if bsXs is not None:
-        return prefix*torch.exp(-(y.unsqueeze(-1)- (m.unsqueeze(-1) + bsXs))**2/2/sigma2)*(y.unsqueeze(-1)- (m.unsqueeze(-1) + bsXs))/sigma2
+        v = (y.unsqueeze(-1)- (m.unsqueeze(-1) + bsXs))/sigma
+        v2 = (y.unsqueeze(-1)- (m.unsqueeze(-1) + bsXs))/sigma2
+        zidx = (y.unsqueeze(-1) > b + (m.unsqueeze(-1) + bsXs)) | (y.unsqueeze(-1) < a + (m.unsqueeze(-1) + bsXs))
     else:
-        return prefix*torch.exp(-(y-m)**2/2/sigma2)*(y-m)/sigma2
+        v = (y-m)/sigma
+        v2 = (y-m)/sigma2
+        zidx = (y > b + m) | (y < a+m)
+    tv = torchstnpdf(v)*v2/Z
+    tv[zidx] = 0
+    return tv
 
 
-def fn22(y, m, sigma=sigma):
-    pi = torch.tensor([np.pi])
+def ftn22(y, m, bsXs=None, sigma=sigma, a=-100, b=100):
     sigma2 = sigma**2
-    prefix = torch.sqrt(1/pi/2/sigma2)
-    expitm = torch.exp(-(y-m)**2/2/sigma2)
-    v =  (y-m)/sigma2
-    linitm = v**2-1/sigma2
-    return prefix*linitm*expitm
+    Z = (STN.cdf(b/sigma) - STN.cdf(a/sigma)) * sigma
+    if bsXs is not None:
+        v = (y.unsqueeze(-1)- (m.unsqueeze(-1) + bsXs))/sigma
+        v2 = ((y.unsqueeze(-1)- (m.unsqueeze(-1) + bsXs))/sigma2)**2 - 1/sigma2
+        zidx = (y.unsqueeze(-1) > b + (m.unsqueeze(-1) + bsXs)) | (y.unsqueeze(-1) < a + (m.unsqueeze(-1) + bsXs))
+    else:
+        v = (y-m)/sigma
+        v2 = ((y-m)/sigma2)**2 - 1/sigma2
+        zidx = (y > b + m) | (y < a+m)
+    tv = torchstnpdf(v)*v2/Z
+    tv[zidx] = 0
+    return tv
 
 
 n = 100
@@ -65,9 +120,11 @@ beta0 = torch.cat((torch.tensor([1.0, 0, 2, 0, 3, 4, 5]), torch.zeros(p-7)))
 bTheta0 = genbTheta(n, m) * 7
 M = bTheta0 + X.matmul(beta0)
 Y = genYnorm(X, bTheta0, beta0, sigma=sigma)
+#Y = genYtnorm(X, bTheta0, beta0, -100, 100,sigma=sigma)
 R = genR(Y)
 # print(X.matmul(beta0).abs().mean(), bTheta0.abs().mean())
 print(R.sum()/R.numel())
+sda
 sXs = genXdis(N, p, type="mvnorm", sigmax=sigmax) 
 conDenfs = [fn, fn2, fn22]
 # etascaleinv = missdepLpbbLoop(bTheta0, beta0, conDenfs, X, Y, R, sXs)
